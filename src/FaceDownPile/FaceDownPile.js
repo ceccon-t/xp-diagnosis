cde0bb41c5c054bd1a4bb9bb4489f4fd8af0c70d
Tiago Ceccon <ceccon.t@hotmail.com>
2021-01-27 22:49:07 -0300

    Add action hints on decks

--- src/FaceDownPile/FaceDownPile.js
+++ src/FaceDownPile/FaceDownPile.js
@@ -4,7 +4,7 @@ import './FaceDownPile.css';
 const FaceDownPile = (props) => {
     return(
         <div className="FaceDownPile">
-            <div className="card-back" onClick={props.pick}>
+            <div className="card-back" onClick={props.pick} title="Pick card from this deck">
                 <img src={props.displayImage} />
             </div>
             <p>{props.remaining}</p>
